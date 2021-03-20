e21931fdfbd1b66fa97a69bb35ddcdc18f79d70c
David Alaka <alakadavid05@gmail.com>
2021-03-20 11:04:37 +0100

    cng

--- functions/email.js
+++ functions/email.js
@@ -10,17 +10,6 @@ const transporter = nodemailer.createTransport({
       pass: PASS
     }
   });
-  let HEADERS = {
-    'Access-Control-Allow-Headers': 'Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Origin',
-    'Content-Type': 'application/json', //optional
-    'Access-Control-Allow-Methods': 'POST, OPTIONS',
-    'Access-Control-Max-Age': '8640'
-  }
-  
-  //This solves the "No ‘Access-Control-Allow-Origin’ header is present on the requested resource."
-  
-  HEADERS['Access-Control-Allow-Origin'] = '*'
-  HEADERS['Vary'] = 'Origin'
 
 exports.handler = async (event,context)=>{
 
@@ -67,7 +56,6 @@ try {
     
     await transporter.sendMail({...data})
     return {
-        HEADERS,
             statusCode:200,
             body:JSON.stringify(`${message}`)
         }
@@ -76,7 +64,7 @@ console.log(res)
 } catch (error) {
     
     return {
-        HEADERS,
+       
             statusCode:400,
             body:'error'
         }
